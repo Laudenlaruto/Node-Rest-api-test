0e83cb8385c1a24dd3e4914daecc5036165f8062
lauden <ben_sanvoisin@hotmail.fr>
2018-03-16 10:04:58 +0100

    Test app

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-var handler = require('./handler');
-var db = require('./db');
+const connectToDatabase = require('./db');
+const Note = require('./models/Note');
+require('dotenv').config({ path: './variables.env' });
 
 app.get('/', function(req, res) {
   res.send({
@@ -14,6 +15,22 @@ app.post('/', function(req, res) {
     "Output": "Hello World!"
   });
 });
+app.post('/notes', function(event, err){
+  connectToDatabase()
+    .then(() => {
+      Note.create(JSON.parse(event.body))
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not create the note.'
+        }));
+    });
+});
+
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
--- handler.js
+++ handler.js
@@ -1,23 +1,9 @@
 'use strict';
-const connectToDatabase = require('./db');
-const Note = require('./models/Note');
-require('dotenv').config({ path: './variables.env' });
+
 module.exports.create = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  connectToDatabase()
-    .then(() => {
-      Note.create(JSON.parse(event.body))
-        .then(note => callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(note)
-        }))
-        .catch(err => callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'Could not create the note.'
-        }));
-    });
+
 };
 
 module.exports.getOne = (event, context, callback) => {
